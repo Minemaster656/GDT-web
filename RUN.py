56b725eebda61b932f0a7f1798ffb726e7bd1f7f
minemaster <glebbasmanov123@gmail.com>
2024-02-28 16:55:50 +0400

    Информативный принт

--- RUN.py
+++ RUN.py
@@ -2,4 +2,7 @@ import os
 
 import Data
 
-os.system(f"uvicorn main:app --reload --port {Data.PORT}")
+# os.system(f"uvicorn main:app --reload --port {Data.PORT}")
+print("Короче, консольный запуск не работает, запускайте main.py.\n"
+      "Если вам выкинуло ошибку, попробуйте запустить SETUP.py и после этого запустить main.py.\n"
+      "А ну и библиотеки с помощью pip install -r requirements.txt установите")
